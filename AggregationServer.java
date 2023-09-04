359ddb76bcb9122fc5c043da870d584b20f8de46
clariechek <chek.clarie@gmail.com>
2023-09-04 09:59:59 +0930

    single client server

--- AggregationServer.java
+++ AggregationServer.java
@@ -5,13 +5,14 @@
  */
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class AggregationServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int portNumber = Integer.parseInt(args[0]);
 
         try (
--- WeatherProtocol.java
+++ WeatherProtocol.java
@@ -4,6 +4,48 @@
  */
 
 public class WeatherProtocol {
-    
+    private static final int WAITING = 0;
+    private static final int CS_CONNECTED = 1;
+    private static final int RECEIVED_REQUEST = 2;
+    private static final int CS_UPDATE_SUCCESS = 3;
+    private static final int INVALID_REQUEST = 4;
+    private static final int CS_NO_CONTENT = 5;
+    // private static final int CS_INVALID_CONTENT = 6;
+
+    private int state = WAITING;
+
+    public String processInput(String theInput) {
+        String theOutput = null;
+
+        if (state == WAITING) {
+            theOutput = "201 - HTTP_CREATED";
+            state = CS_CONNECTED;
+        } else if (state == CS_CONNECTED || state == CS_UPDATE_SUCCESS) {
+            if (theInput.equals("GET")) {
+                theOutput = "200 - HTTP_OK";
+                state = RECEIVED_REQUEST;
+            } else if (theInput.equals("PUT")) {
+                theOutput = "200 - HTTP_OK";
+                state = RECEIVED_REQUEST;
+            } else {
+                theOutput = "400 - HTTP_BAD_REQUEST";
+                state = INVALID_REQUEST;
+            }
+        } else if (state == RECEIVED_REQUEST) {
+            if (theInput == null) {
+                theOutput = "204 - HTTP_NO_CONTENT";
+                state = CS_NO_CONTENT;
+            } else {
+                theOutput = "200 - HTTP_OK";
+                state = CS_UPDATE_SUCCESS;
+            }
+            // else if (checkJSONInput(theInput) == false) {
+            //     theOutput = "500 - HTTP_INTERNAL_SERVER_ERROR";
+            //     state = CS_INVALID_CONTENT;
+            // } 
+        }
+        
+        return theOutput;
+    }
 }
  
